9cba5061b6f18072921566094c573dc7186cb337
kyliem41 <krhodus@student.neumont.edu>
2025-05-09 15:39:03 -0600

    edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 import HomePage from './components/homepage';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+// import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function App() {
   return (
